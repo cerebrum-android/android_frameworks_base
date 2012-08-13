f3bac8dd05b8996a4363bfeeb64d0a2d349568a0
Mihai Costea <mihai.andrei.costea@gmail.com>
2012-08-13 14:23:44 +0300

    Remove some more unneeded telephony classes

--- telephony/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
+++ telephony/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -1222,9 +1222,7 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
         } catch (Exception e){
         }
 
-        boolean mvnoRoaming = Settings.System.getInt(phone.getContext().getContentResolver(),
-            Settings.System.MVNO_ROAMING, 0) == 1;
-        return gsmRoaming && !(equalsMcc && (equalsOnsl || equalsOnss || mvnoRoaming));
+        return gsmRoaming && !(equalsMcc && (equalsOnsl || equalsOnss));
     }
 
     private static int twoDigitsAt(String s, int offset) {
