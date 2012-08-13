d0bc2299f7dc9dfa23a2090442e553c2890a6ee0
Mihai Costea <mihai.andrei.costea@gmail.com>
2012-08-13 23:42:27 +0300

    Add DynamicMemoryManagerService and patch RS implementation

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -111,6 +111,7 @@ class ServerThread extends Thread {
 
         LightsService lights = null;
         PowerManagerService power = null;
+        DynamicMemoryManagerService dmm = null;
         BatteryService battery = null;
         AlarmManagerService alarm = null;
         NetworkManagementService networkManagement = null;
